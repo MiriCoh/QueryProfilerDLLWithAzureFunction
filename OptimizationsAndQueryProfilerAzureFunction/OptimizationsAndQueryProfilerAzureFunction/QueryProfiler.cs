aa386107ab7cd48e15d874a1ed68c251db428dd6
user1 <user1@DESKTOP-49VMUKQ>
2022-08-24 00:04:38 +0300

    The DLL with Azure Function

--- OptimizationsAndQueryProfilerAzureFunction/OptimizationsAndQueryProfilerAzureFunction/QueryProfiler.cs
+++ OptimizationsAndQueryProfilerAzureFunction/OptimizationsAndQueryProfilerAzureFunction/QueryProfiler.cs
@@ -2,10 +2,9 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using QueryProfiler.Optimization;
 using QueryProfiler.Profile;
 using QueryProfiler;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
 namespace OptimizationsAndQueryProfilerAzureFunction
 {
@@ -13,7 +12,7 @@ namespace OptimizationsAndQueryProfilerAzureFunction
     {
         [FunctionName("GetQueryProfiler")]
         public static async Task<ProfileScheme> Run(
-              [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetQueryProfiler/{query}")] string query,
+              [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetQueryProfiler/{query}")] HttpRequest req,string query,
               ILogger log)
         {
             log.LogInformation("C# HTTP trigger function GetQueryProfiler processed a request.");
